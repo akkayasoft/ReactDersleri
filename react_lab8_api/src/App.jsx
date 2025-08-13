96e8efdc7d437e6b3063dd6758f8cf9e6aee30fc
akkayasoft <akkayasoft@gmail.com>
2025-08-13 08:48:59 +0300

    api veri çekme uygulama bitti

---
+++ react_lab8_api/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import FetchData from './Components/FetchData'
+
+function App() {
+  
+
+  return (
+    <>
+      <FetchData/>
+    </>
+  )
+}
+
+export default App
---
+++ react_lab8_api/src/Components/FetchData.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import useFetch from './UseFetch'
+import './FetchData.css'
+
+const FetchData=()=>{
+    const [data]=useFetch('https://api.npoint.io/9045c260b1565daa9e15');
+    console.log(data);
+return(
+    <>
+    <h1 className="useFetch_heading">API den Veri Çekme</h1>
+    <ul className="list_data_main">
+        {data && data.map((e,index)=>(
+            <li key={index} className="list_data">
+                <h3>{e.name}</h3>
+                <p>
+                    <strong>Önemi:</strong>
+                    {e.importance}
+                </p>
+                <p>
+                    <strong>Yararları:</strong>
+                    {e.benefits}
+                </p>
+                <p>
+                    <strong>Yeme Zamanları:</strong>
+                    {e.best_time_to_intake}
+                </p>
+            </li>
+        ))
+        }
+    </ul>
+    </>
+)
+}
+
+export default FetchData
+
